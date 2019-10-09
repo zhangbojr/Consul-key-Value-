363aa0d6f623141972032d914309cddd619e037d
Focus <403127140@qq.com>
2019-10-09 11:46:50 +0800

    Add files via upload
    Consul/key/Value 统一配置中心

---
+++ Bo.ServiceB/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Winton.Extensions.Configuration.Consul;
+
+namespace Bo.ServiceB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    hostingContext.Configuration = config.Build();
+                    string consul_url = hostingContext.Configuration["Consul_Url"];
+                    config.AddConsul(
+                                $"{env.ApplicationName}/appsettings.{env.EnvironmentName}.json",
+                                cancellationTokenSource.Token,
+                                options =>
+                                {
+                                    options.Optional = true;
+                                    options.ReloadOnChange = true;
+                                    options.OnLoadException = exceptionContext => { exceptionContext.Ignore = true; };
+                                    options.ConsulConfigurationOptions = cco => { cco.Address = new Uri(consul_url); };
+                                }
+                                ).AddEnvironmentVariables();
+
+                    hostingContext.Configuration = config.Build();
+                }).UseUrls("http://localhost:5012")
+                 .UseStartup<Startup>();
+        }
+    }
+}
